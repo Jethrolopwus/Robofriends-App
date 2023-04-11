18541a82d16e3d3bbddf628c02661eda668df0a3
Jethro Lopwus <lopwusjethro92@gmail.com>
2023-04-11 14:44:12 +0100

    first commit

---
+++ src/robots.js
@@ -0,0 +1,85 @@
+export const  robots = [
+    {
+        id: 1,
+        name: 'Jethro B',
+        username: 'Jay B',
+        email: 'jay@gmail.com'
+    },
+
+    {
+        id: 2,
+        name: 'Peter G',
+        username: 'Mr P',
+        email: 'peter@gmail.com'
+    },
+
+    {
+        id: 3,
+        name: 'Emma James',
+        username: 'emmy',
+        email: 'emma@gmail.com'
+    },
+
+
+    
+    {
+        id: 4,
+        name: 'Abdul Mash',
+        username: 'Abmash',
+        email: 'mash@gmail.com'
+    },
+
+    
+    {
+        id: 5,
+        name: 'Lovet Bonet',
+        username: 'Lovet',
+        email: 'lovet@gmail.com'
+    },
+
+    
+    {
+        id: 6,
+        name: 'Ben Jeree',
+        username: 'Jerry',
+        email: 'jerry@gmail.com'
+    },
+
+     
+    {
+        id: 7,
+        name: 'Bob Marley',
+        username: 'Marley',
+        email: 'maerley@gmail.com'
+    },
+
+     
+    {
+        id: 8,
+        name: 'JohnDoe',
+        username: 'John',
+        email: 'john@gmail.com'
+    },
+
+     
+    {
+        id: 9,
+        name: 'James Brad',
+        username: 'Brad',
+        email: 'brad@gmail.com'
+    },
+
+     
+    {
+        id: 10,
+        name: 'Emly Jan',
+        username: 'Emly',
+        email: 'emly@gmail.com'
+    },
+
+
+
+
+
+
+];
