e19c44df5ded095c88f9dbc29e2e9e5a76171c4d
Jethro Lopwus <lopwusjethro92@gmail.com>
2023-04-21 12:45:46 +0100

    modifeid app.js file in container folder

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,42 +13,35 @@ class App extends Component {
             robots: [],
     searchfield: '',
         }
-    
     } 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(Response =>Response.json())
         .then(users=> this.setState({robots: users}));
-    
     }
     onSearchChange = (event) => {
         this.setState({searchfield: event.target.value})
     }
     
     render () {
-    
-    const filteredRobots = this.state.robots.filter(robots => {
-        return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-
+        const {robots, searchfield} = this.state;
+    const filteredRobots = robots.filter(robot => {
+        return robot.name.toLowerCase().includes(searchfield.toLowerCase())
     })
-    if (this.state.robots.length === 0){
-        return <h1>Loading</h1>
-    } else{
-        return (
+
+    return !robots.length ?
+
+     <h1>Loading</h1> :
+
+        (
             <div className='tc'>
                 <h1 className='f2'>RoboFriends</h1>
                 <SearchBox searchchange = {this.onSearchChange}/>
                 <Scroll>
                     <CardList robots = {filteredRobots}/>
                 </Scroll>
-            </div>
-            
-        );
-
-    }
-
-    }
-   
+            </div>  
+        )
+    }    
 }
-
 export default App;
